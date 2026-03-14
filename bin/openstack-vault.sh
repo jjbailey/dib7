62c581749ca84f77f23bb41973983ed918cca308
Jack Bailey <jack@internetguy.net>
2026-03-14 09:41:51 -0700

    v0.1.2 (#6)
    * try fixing mermaid for github
    
    * bug: adds swap to wrong /etc/fstab
    
    * improve vault helper scripts

--- bin/openstack-vault.sh
+++ bin/openstack-vault.sh
@@ -4,7 +4,16 @@
 
 set -e
 
+script_dir="$(CDPATH= builtin cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && builtin pwd -P)"
+project_dir="$(CDPATH= builtin cd -- "$script_dir/.." >/dev/null && builtin pwd -P)"
+
 [ -z "$VAULT" ] && VAULT="vaults/openstack.yml"
+
+case "$VAULT" in
+    /*) ;;
+    *) VAULT="$project_dir/$VAULT" ;;
+esac
+
 [ -s "$VAULT" ] && action=edit || action=create
 
 ansible-vault $action --encrypt-vault-id=default $VAULT
